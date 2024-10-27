865e33c86fc4f8933b93df1481d7e014a0ed9d1b
Matthew Ibbetson <mmibbetson@gmail.com>
2024-10-27 14:20:02 +0200

    feat: draft get_frontmatter signature

--- src/frontmatter.rs
+++ src/frontmatter.rs
@@ -1,1 +1,10 @@
+use crate::{config::FrontmatterConfig, filename::FilenameDetails};
 
+pub fn get_frontmatter(filename_details: &FilenameDetails, config: &FrontmatterConfig) -> String {
+    config
+        .segment_order
+        .iter()
+        .map(|segment| process_segment(segment, filename_details, config))
+        .collect::<Vec<_>>()
+        .concat()
+}
