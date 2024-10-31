328334c5366d130258207b2d37a04081c6e1eb63
Matthew Ibbetson <mmibbetson@gmail.com>
2024-10-31 09:14:07 +0200

    feat: draft clap derive

--- src/args.rs
+++ src/args.rs
@@ -11,3 +11,125 @@
 // When --regenerate-identifier is false, check get_identifier() and if the file has an identifier
 // we will use that to get the creation_time and identifier_arg. This way, frontmatter.rs can be
 // agnostic wrt the identifier arg.
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "dn", version = "0.1", about = "A command to manage notes following the Denote naming scheme.")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// Display help information
+    #[arg(short, long)]
+    help: bool,
+
+    /// Display version information
+    #[arg(short, long)]
+    version: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new note
+    New {
+        /// Generate or regenerate frontmatter
+        #[arg(short = 'G', long)]
+        generate_frontmatter: bool,
+
+        /// Directory to save the note
+        #[arg(short = 'd', long)]
+        directory: Option<String>,
+
+        /// Order of elements
+        #[arg(short = 'o', long)]
+        order: Option<String>,
+
+        /// Frontmatter order
+        #[arg(short = 'O', long)]
+        frontmatter_order: Option<String>,
+
+        /// Config file location
+        #[arg(short = 'c', long)]
+        config: Option<String>,
+
+        /// Template file
+        #[arg(short = 'T', long)]
+        template: Option<String>,
+
+        /// Frontmatter format
+        #[arg(short = 'F', long)]
+        frontmatter_format: Option<String>,
+
+        /// Signature to use
+        #[arg(short = 's', long)]
+        signature: Option<String>,
+
+        /// Title for the note
+        #[arg(short = 't', long)]
+        title: Option<String>,
+
+        /// File extension
+        #[arg(short = 'e', long)]
+        extension: Option<String>,
+
+        /// Keywords for the note
+        #[arg(short = 'k', long)]
+        keywords: Option<String>,
+    },
+    
+    /// Rename an existing note
+    Rename {
+        /// Path to the input file
+        input: String,
+
+        /// Generate identifier even if there is an existing one
+        #[arg(short = 'I', long)]
+        regenerate_identifier: bool,
+
+        /// Rename based on frontmatter values
+        #[arg(short = 'f', long)]
+        frontmatter: bool,
+
+        /// Generate or regenerate frontmatter
+        #[arg(short = 'G', long)]
+        generate_frontmatter: bool,
+
+        /// Order of elements
+        #[arg(short = 'o', long)]
+        order: Option<String>,
+
+        /// Frontmatter order
+        #[arg(short = 'O', long)]
+        frontmatter_order: Option<String>,
+
+        /// Config file location
+        #[arg(short = 'c', long)]
+        config: Option<String>,
+
+        /// Frontmatter format
+        #[arg(short = 'F', long)]
+        frontmatter_format: Option<String>,
+
+        /// Signature to use
+        #[arg(short = 's', long)]
+        signature: Option<String>,
+
+        /// File extension
+        #[arg(short = 'e', long)]
+        extension: Option<String>,
+
+        /// Keywords for the note
+        #[arg(short = 'k', long)]
+        keywords: Option<String>,
+
+        /// Add keywords
+        #[arg(short = 'A', long)]
+        add_keywords: Option<String>,
+
+        /// Remove keywords
+        #[arg(short = 'R', long)]
+        remove_keywords: Option<String>,
+    },
+}
+
