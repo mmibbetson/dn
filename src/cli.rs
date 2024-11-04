d61b7b88e66040e4fafc09a6a6499cdc3799a5d5
Matthew Ibbetson <mmibbetson@gmail.com>
2024-11-04 11:43:30 +0200

    refactor: main in more functional style

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(
     name = "dn",
-    version = "0.1",
+    version = "0.1.1",
     about = "A command to manage notes following the Denote naming scheme."
 )]
 pub struct Cli {
@@ -20,45 +20,52 @@ pub struct Cli {
 }
 
 // TODO: Reconsider clone.
-// TODO: Give explicit long names and better-differentiated struct value names.
 #[derive(Subcommand, Clone)]
 pub enum Commands {
     /// Create a new note
     New {
+        /// Print the absolute path of the created file
+        #[arg(short = 'p', long = "print")]
+        print_path: bool,
+
         /// Generate or regenerate frontmatter
-        #[arg(short = 'G', long)]
+        #[arg(short = 'G', long = "generate-frontmatter")]
         generate_frontmatter: bool,
 
         /// Directory to save the note
-        #[arg(short = 'd', long, value_name = "DIRECTORY")]
+        #[arg(short = 'd', long = "directory", value_name = "PATH")]
         directory: Option<String>,
 
         /// Config file location
-        #[arg(short = 'c', long)]
+        #[arg(short = 'c', long = "config", value_name = "PATH")]
         config: Option<String>,
 
         /// Template file
-        #[arg(short = 'T', long)]
+        #[arg(short = 'T', long = "template", value_name = "PATH")]
         template: Option<String>,
 
         /// Frontmatter format
-        #[arg(short = 'F', long)]
+        #[arg(
+            short = 'F',
+            long = "frontmatter-format",
+            value_name = "Text|YAML|TOML|Org"
+        )]
         frontmatter_format: Option<String>,
 
         /// Signature to use
-        #[arg(short = 's', long)]
+        #[arg(short = 's', long = "signature", value_name = "SIGNATURE")]
         signature: Option<String>,
 
         /// Title for the note
-        #[arg(short = 't', long)]
+        #[arg(short = 't', long = "title", value_name = "TITLE")]
         title: Option<String>,
 
         /// File extension
-        #[arg(short = 'e', long)]
+        #[arg(short = 'e', long = "extension", value_name = "EXTENSION")]
         extension: Option<String>,
 
         /// Keywords for the note
-        #[arg(short = 'k', long)]
+        #[arg(short = 'k', long = "keywords", value_name = "KEYWORD(S)")]
         keywords: Option<String>,
     },
 
@@ -67,44 +74,56 @@ pub enum Commands {
         /// Path to the input file
         input: String,
 
+        /// Print the absolute path of the created file
+        #[arg(short = 'p', long = "print")]
+        print_path: bool,
+
         /// Generate identifier even if there is an existing one
-        #[arg(short = 'I', long)]
+        #[arg(short = 'I', long = "regenerate-identifier")]
         regenerate_identifier: bool,
 
         /// Rename based on frontmatter values
-        #[arg(short = 'f', long)]
+        #[arg(short = 'f', long = "frontmatter")]
         frontmatter: bool,
 
         /// Generate or regenerate frontmatter
-        #[arg(short = 'G', long)]
+        #[arg(short = 'G', long = "generate-frontmatter")]
         generate_frontmatter: bool,
 
         /// Config file location
-        #[arg(short = 'c', long)]
+        #[arg(short = 'c', long = "config", value_name = "PATH")]
         config: Option<String>,
 
         /// Frontmatter format
-        #[arg(short = 'F', long)]
+        #[arg(
+            short = 'F',
+            long = "frontmatter-format",
+            value_name = "Text|YAML|TOML|Org"
+        )]
         frontmatter_format: Option<String>,
 
         /// Signature to use
-        #[arg(short = 's', long)]
+        #[arg(short = 's', long = "signature", value_name = "SIGNATURE")]
         signature: Option<String>,
 
-        /// File extension
-        #[arg(short = 'e', long)]
-        extension: Option<String>,
+        /// Title for the note
+        #[arg(short = 't', long = "title", value_name = "TITLE")]
+        title: Option<String>,
 
         /// Keywords for the note
-        #[arg(short = 'k', long)]
+        #[arg(short = 'k', long = "keywords", value_name = "KEYWORDS")]
         keywords: Option<String>,
 
         /// Add keywords
-        #[arg(short = 'A', long)]
+        #[arg(short = 'A', long = "add-keywords", value_name = "KEYWORDS")]
         add_keywords: Option<String>,
 
         /// Remove keywords
-        #[arg(short = 'R', long)]
+        #[arg(short = 'R', long = "remove-keywords", value_name = "KEYWORDS")]
         remove_keywords: Option<String>,
+
+        /// File extension
+        #[arg(short = 'e', long = "extension", value_name = "EXTENSION")]
+        extension: Option<String>,
     },
 }
--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,12 @@ mod filename;
 mod format;
 mod metadata;
 
-// Top-down draft using api
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
         cli::Commands::New {
+            print_path,
             generate_frontmatter,
             directory,
             config,
@@ -36,13 +36,11 @@ fn main() {
             extension,
             keywords,
         } => {
-            let creation_time = chrono::Local::now();
-
             let config_path = config.map_or(get_default_config_dir(), PathBuf::from);
             let config_content = read_config(config_path).unwrap_or_default();
-            // WARN: This clones the command struct and then matches on it again...
             let config_final = update_config_with_cli_args(cli.command.clone(), &config_content);
 
+            let creation_time = chrono::Local::now();
             let metadata = FileMetadataBuilder::new(creation_time)
                 .with_signature(signature)
                 .with_title(title)
@@ -50,88 +48,83 @@ fn main() {
                 .with_extension(extension)
                 .build(&config_final.file);
 
-            let filename = metadata.to_filename(&config_content.file);
-
-            let frontmatter =
-                generate_frontmatter.then(|| metadata.to_frontmatter(config_final.frontmatter));
+            let filename = metadata.to_filename(&config_final.file).to_string();
+            let frontmatter = generate_frontmatter.then(|| {
+                metadata
+                    .to_frontmatter(&config_final.frontmatter)
+                    .to_string()
+            });
 
             let template_content = template.map(|tmp| get_template(tmp, config_final.file));
 
-
-            let path = directory.map_or(get_path(config_final.file.directory), PathBuf::from);
+            let path = directory
+                .map_or(get_path(config_final.file.directory), PathBuf::from)
+                .join(filename);
 
             let content = get_content(frontmatter, template_content);
 
             fs::write(path, content);
+
+            print_path.then(|| print!("{}", path));
         }
         cli::Commands::Rename {
             input,
+            print_path,
             regenerate_identifier,
             frontmatter,
             generate_frontmatter,
             config,
             frontmatter_format,
             signature,
+            title,
             extension,
             keywords,
             add_keywords,
             remove_keywords,
         } => {
             let input_path = PathBuf::from(input);
-            let input_content = fs::read_to_string(input_path);
+            // WARN: Unwrapping may panic.
+            let input_content = fs::read_to_string(input_path).unwrap();
 
             let config_path = config.map_or(get_default_config_dir(), PathBuf::from);
             let config_content = read_config(config_path).unwrap_or_default();
-            // WARN: This clones the command struct and then matches on it again...
             let config_final = update_config_with_cli_args(cli.command.clone(), &config_content);
 
-            // Get the filename as a string, fallback to empty string if invalid UTF-8 or no filename
             let file_name = PathBuf::from(input)
                 .file_name()
-                .and_then(|os_str| os_str.to_str())
-                .unwrap() // WARN: Smells funny.
+                .and_then(|o| o.to_str())
+                // WARN: Unwrap may panic.
+                .unwrap()
                 .to_string();
 
-            // Get initial filename data
             let mut existing_filename = file_name.to_filename(&config_final.file);
             let mut parse_time = derive_creation_time(&existing_filename.identifier);
 
-            // If frontmatter is true, override with frontmatter values where they exist
-            if *frontmatter {
-                let existing_frontmatter = file_name.to_frontmatter();
+            frontmatter.then(|| {
+                // NOTE: We take the first 6 lines only because at most that is how many lines
+                // of dn frontmatter will be present (in the case of YAML and TOML with all fields)
+                let existing_frontmatter = input_content.lines().take(6).collect().to_frontmatter();
+                existing_frontmatter.title.map(|t| existing_filename.title);
+                existing_frontmatter.datetime.map(|d| parse_time = d);
+                existing_frontmatter
+                    .keywords
+                    .map(|k| existing_filename.keywords);
+                existing_frontmatter
+                    .identifier
+                    .map(|i| existing_filename.identifier = i);
+            });
 
-                if let Some(dt) = existing_frontmatter.datetime {
-                    parse_time = dt;
-                }
-
-                if let Some(id) = existing_frontmatter.identifier {
-                    existing_filename.identifier = existing_frontmatter.identifier;
-                }
-
-                existing_filename.signature = existing_frontmatter
-                    .signature
-                    .or(existing_filename.signature);
-                existing_filename.title = existing_frontmatter.title.or(existing_filename.title);
-                existing_filename.keywords =
-                    existing_frontmatter.keywords.or(existing_filename.keywords);
-
-                if !existing_frontmatter.extension.is_empty() {
-                    existing_filename.extension = existing_frontmatter.extension;
-                }
-            }
-
-            // Override with provided CLI values where they exist
-            if let Some(sig) = signature {
-                existing_filename.signature = Some(sig.to_string());
-            }
-
-            if let Some(kw) = keywords {
-                existing_filename.keywords = Some(kw.to_string());
-            }
-
-            if let Some(ext) = extension {
-                existing_filename.extension = ext.to_string();
-            }
+            // WARN: Side-effecting mutation.
+            signature.map(|s| existing_filename.signature = Some(s));
+            title.map(|t| existing_filename.title = Some(t));
+            keywords.map(|k| existing_filename.keywords = Some(k));
+            add_keywords.map(|k| {
+                // k needs to be deserialised and sanitised then concatenated onto existing_filename.keywords.
+            });
+            remove_keywords.map(|k| {
+                // k needs to be deserialised and sanitised then existing_filename.keywords.iter().filter(!k.contains)
+            });
+            extension.map(|e| existing_filename.extension = e);
 
             let metadata = FileMetadataBuilder::new(parse_time)
                 .with_identifier(&Some(existing_filename.identifier))
@@ -139,16 +132,25 @@ fn main() {
                 .with_title(&existing_filename.title)
                 .with_keywords(&existing_filename.keywords)
                 .with_extension(&Some(existing_filename.extension))
-                .build(&config_final.file); // WARN: Possible code smell. Why does metadata take a FILE config?
+                // WARN: Possible code smell. Why does metadata take a FILE config?
+                .build(&config_final.file);
 
-            let new_filename = metadata.to_filename(&config_content.file);
+            let new_filename = metadata.to_filename(&config_final.file).to_string();
+            let new_frontmatter = generate_frontmatter.then(|| {
+                metadata
+                    .to_frontmatter(config_final.frontmatter)
+                    .to_string()
+            });
 
-            let new_frontmatter =
-                generate_frontmatter.then(|| metadata.to_frontmatter(config_content.frontmatter));
-
+            // WARN: Unwrap may panic.
+            let output_path = input_path.parent().unwrap().join(new_filename);
             let output_content = get_rename_content(new_frontmatter, input_content);
 
             fs::write(input_path, output_content);
+            fs::rename(input_path, output_path);
+
+            // WARN: Unwrap may panic.
+            print_path.then(|| print!("{}", output_path.to_str().unwrap()));
         }
     }
 }
@@ -163,38 +165,28 @@ fn update_config_with_cli_args(args: cli::Commands, original_config: &Config) ->
             template,
             frontmatter_format,
             extension,
+            print_path: _,
             config: _,
             signature: _,
             title: _,
             keywords: _,
         } => {
-            if generate_frontmatter {
-                config.frontmatter.enabled = generate_frontmatter;
-            };
+            generate_frontmatter.then(|| config.frontmatter.enabled = generate_frontmatter);
 
-            if let Some(dir) = directory {
-                config.file.directory = PathBuf::from(dir);
-            };
+            directory.map(|d| config.file.directory = PathBuf::from(d));
+            extension.map(|e| config.file.default_extension = e);
+            template.map(|t| config.file.template_path = Some(PathBuf::from(t)));
 
-            if let Some(ext) = extension {
-                config.file.default_extension = ext;
-            };
-
-            if let Some(tmp) = template {
-                config.file.template_path = Some(PathBuf::from(tmp));
-            }
-
-            if let Some(fmt) = frontmatter_format {
-                config.frontmatter.format = match fmt.to_lowercase().as_str() {
+            frontmatter_format.map(|f| {
+                config.frontmatter.format = match f.to_lowercase().as_str() {
                     "text" => config::FrontmatterFormat::Text,
                     "yaml" => config::FrontmatterFormat::YAML,
                     "toml" => config::FrontmatterFormat::TOML,
                     "org" => config::FrontmatterFormat::Org,
-                    // WARN: Panicking.
-                    // TODO: Maybe throw anyhow error alert invalid format, or something?
+                    // WARN: Panicking. Maybe throw anyhow error alert invalid format, or something?
                     _ => panic!("Invalid frontmatter format provided, must be one of: text, yaml, toml, org"),
                 };
-            }
+            });
         }
         cli::Commands::Rename {
             regenerate_identifier,
@@ -202,90 +194,33 @@ fn update_config_with_cli_args(args: cli::Commands, original_config: &Config) ->
             frontmatter_format,
             extension,
             input: _,
+            print_path: _,
             frontmatter: _,
             config: _,
             signature: _,
+            title: _,
             keywords: _,
             add_keywords: _,
             remove_keywords: _,
         } => {
-            if regenerate_identifier {
-                config.file.regenerate_identifier = regenerate_identifier;
-            };
+            regenerate_identifier
+                .then(|| config.file.regenerate_identifier = regenerate_identifier);
+            generate_frontmatter.then(|| config.frontmatter.rewrite = generate_frontmatter);
 
-            if generate_frontmatter {
-                config.frontmatter.rewrite = generate_frontmatter;
-            };
+            extension.map(|e| config.file.default_extension = e);
 
-            if let Some(ext) = extension {
-                config.file.default_extension = ext;
-            };
-
-            if let Some(fmt) = frontmatter_format {
-                config.frontmatter.format = match fmt.to_lowercase().as_str() {
+            frontmatter_format.map(|f| {
+                config.frontmatter.format = match f.to_lowercase().as_str() {
                     "text" => config::FrontmatterFormat::Text,
                     "yaml" => config::FrontmatterFormat::YAML,
                     "toml" => config::FrontmatterFormat::TOML,
                     "org" => config::FrontmatterFormat::Org,
-                    _ => panic!("Invalid frontmatter format provided, must be one of: text, yaml, toml, org"), // TODO: throw anyhow error alert invalid format, or something
+                    // WARN: Panicking. Maybe throw anyhow error alert invalid format, or something?
+                    _ => panic!("Invalid frontmatter format provided, must be one of: text, yaml, toml, org"),
                 };
-            }
+            });
         }
     };
 
     config
 }
-
-// When getting --add-keywords or --remove-keywords we want to modify the keywords_arg
-// 1. Take existing keywords_arg (-k>filename_parse>None)
-// 2. format!("{}_{}", keywords_arg, add_keywords_arg)
-// 3. split words in remove_keywords_arg
-// 4. iterate over formatted string, remove instances of remove keywords arg words from [3]
-// 5. collect properly into nice single string, to be used as final keywords_arg value
-
-// When renaming a file, be sure to parse the filename correctly out of the input path
-// and rejoin if necessary when writing to disk or w/e.
-
-// When --regenerate-identifier is false, check get_identifier() and if the file has an identifier
-// we will use that to get the creation_time and identifier_arg. This way, frontmatter.rs can be
-// agnostic wrt the identifier arg.
-
-// let example = FileMetadata {
-//     identifier: "20241031T232930",
-//     signature: "ggl210",
-//     title_raw: "Sprint Goals! - 210",
-//     title: "sprint-goals-210",
-//     keywords: vec!["ggl", "client-ADmtars!!", "admin"],
-//     extension: "md",
-//     datetime: chrono::Local::now(),
-// }
-
-// 20241031T232930==GGL210--sprint-goals-210__ggl_client_admin.md
-
-// NOTE: It's important that tags/filetags are named as such rather than keywords
-// This is due to the way various existing programs parse them.
-
-// ---
-// title:      "Sprint Goals - 210"
-// date:       "2024-10-31 23:34:30 +2:00"
-// tags:       ["ggl", "client", "admin"]
-// identifier: "20241031T232930"
-// ---
-
-// +++
-// title =      "Sprint Goals - 210"
-// date =       "2024-10-31 23:34:30 +2:00"
-// tags =       ["ggl", "client", "admin"]
-// identifier = "20241031T232930"
-// +++
-
-// title:      Sprint Goals - 210
-// date:       2024-10-31 23:34:30 +2:00
-// tags:       ggl  client  admin
-// identifier: "20241031T232930"
-// -----------------------------
-
-// #+title:      Sprint Goals - 210
-// #+date:       [2024-10-31 23:34:30 +2:00]
-// #+filetags:   :ggl:client:admin:
-// #+identifier: 20241031T232930
