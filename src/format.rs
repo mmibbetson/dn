8421da65ec241d5a52b1ae1342d3cfea8fe6c0c2
Matthew Ibbetson <mmibbetson@gmail.com>
2024-11-10 18:07:35 +0200

    feat: add capture groups to frontmatter regexes, pull content and frontmatter at the same time, add tests to format.rs

--- src/format.rs
+++ src/format.rs
@@ -6,35 +6,41 @@ const TEXT_FRONTMATTER_DATE: &str = r#"date:\s+(.+)\n|(?:\r\n)"#; // will also u
 const TEXT_FRONTMATTER_KEYWORDS: &str = r#"tags:\s+((?:\S+\s+)*\S+)\n|(?:\r\n)"#; // will also unfortunately match the org filetags
 const TEXT_FRONTMATTER_IDENTIFIER: &str = r#"identifier:\s+(\d{8}T\d{6})\n|(?:\r\n)"#; // will also unfortunately match the org ident
 
-const YAML_FRONTMATTER_CONTAINER: &str = r#"\n---\n"#;
+const YAML_FRONTMATTER_CONTAINER: &str = r#"---\n"#;
 const YAML_FRONTMATTER_TITLE: &str = r#"title:\s+(".+")\n|(?:\r\n)"#;
 const YAML_FRONTMATTER_DATE: &str = r#"date:\s+(.+)\n|(?:\r\n)"#; // will also unfortunately match the org and text dates
-const YAML_FRONTMATTER_KEYWORDS: &str = r#"tags:\s+(\[(?:".+",\s).*".+"\])\n|(?:\r\n)"#;
+const YAML_FRONTMATTER_KEYWORDS: &str = r#"tags:\s+(\[(?:".+",\s+).*".+"\])\n|(?:\r\n)"#;
 const YAML_FRONTMATTER_IDENTIFIER: &str = r#"identifier:\s+("\d{8}T\d{6}")\n|(?:\r\n)"#;
 
-const TOML_FRONTMATTER_CONTAINER: &str = r#"\n+++\n"#;
+const TOML_FRONTMATTER_CONTAINER: &str = r#"+++\n"#;
 const TOML_FRONTMATTER_TITLE: &str = r#"title\s+=\s+(".+")\n|(?:\r\n)"#;
 const TOML_FRONTMATTER_DATE: &str = r#"date\s+=\s+(.+)\n|(?:\r\n)"#;
-const TOML_FRONTMATTER_KEYWORDS: &str = r#"tags\s+=\s+(\[(?:".+",\s).*".+"\])\n|(?:\r\n)"#;
-const TOML_FRONTMATTER_IDENTIFIER: &str = r#"identifier\s=\s+("\d{8}T\d{6}")\n|(?:\r\n)"#;
+const TOML_FRONTMATTER_KEYWORDS: &str = r#"tags\s+=\s+(\[(?:".+",\s+).*".+"\])\n|(?:\r\n)"#;
+const TOML_FRONTMATTER_IDENTIFIER: &str = r#"identifier\s+=\s+("\d{8}T\d{6}")\n|(?:\r\n)"#;
 
+const ORG_SEGMENT_PREFIX: &str = r#"#+"#;
 const ORG_FRONTMATTER_TITLE: &str = r#"#\+title:\s+(.+)\n|(?:\r\n)"#;
 const ORG_FRONTMATTER_DATE: &str = r#"#\+date:\s+(.+)\n|(?:\r\n)"#; // matches any time format
 const ORG_FRONTMATTER_KEYWORDS: &str = r#"#\+filetags:\s+((?::\S+)+:)\n|(?:\r\n)"#;
 const ORG_FRONTMATTER_IDENTIFIER: &str = r#"#\+identifier:\s+(\d{8}T\d{6})\n|(?:\r\n)"#;
 
 /// TODO
-pub fn get_frontmatter_section(content: &str) -> Option<String> {
-    content
+pub fn separate_existing_content(content: &str) -> (Option<String>, Option<String>) {
+    let split = content
         .split_once("\n\n")
-        .or_else(|| content.split_once("\r\n\r\n"))
-        .and_then(|(p, _)| {
-            if is_valid_frontmatter_format(p) {
-                Some(p.to_string())
-            } else {
-                None
-            }
-        })
+        .or_else(|| content.split_once("\r\n\r\n"));
+
+    let frontmatter = split.and_then(|(p, _)| {
+        if is_valid_frontmatter_format(p) {
+            Some(p.to_string())
+        } else {
+            None
+        }
+    });
+
+    let content = split.map(|(_, s)| s.to_string());
+
+    (frontmatter, content)
 }
 
 /// TODO
@@ -48,11 +54,12 @@ fn is_valid_frontmatter_format(content: &str) -> bool {
         first == Some(&YAML_FRONTMATTER_CONTAINER) && last == Some(&YAML_FRONTMATTER_CONTAINER);
     let is_toml =
         first == Some(&TOML_FRONTMATTER_CONTAINER) && last == Some(&TOML_FRONTMATTER_CONTAINER);
-    let is_org = !lines.is_empty() && lines.iter().all(|l| l.starts_with("#+"));
+    let is_org = !lines.is_empty() && lines.iter().all(|l| l.starts_with(&ORG_SEGMENT_PREFIX));
 
     is_text || is_yaml || is_toml || is_org
 }
 
+// TODO: Fix failing yaml/toml tests and be more decisive on the datetime testing & design direction
 #[cfg(test)]
 mod tests {
     use regex::Regex;
@@ -150,19 +157,20 @@ mod tests {
         #[test]
         fn test_container() {
             let re = Regex::new(YAML_FRONTMATTER_CONTAINER).unwrap();
-            assert!(re.is_match("\n---\n"));
-            assert!(!re.is_match("\n--\n"));
-            assert!(!re.is_match("\n----\n"));
+            assert!(re.is_match("---\n"));
+            assert!(!re.is_match("--\n"));
+            assert!(!re.is_match("----\n"));
         }
 
         #[test]
         fn test_title() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(YAML_FRONTMATTER_TITLE).unwrap();
+            let input = "title:   \"my-Test -  A N3w title\"\n";
+            let expected = "\"my-Test -  A N3w title\"";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -174,11 +182,12 @@ mod tests {
         #[test]
         fn test_date() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(YAML_FRONTMATTER_DATE).unwrap();
+            let input = "date:  2024-12-12 @ 12:12:12\n";
+            let expected = "2024-12-12 @ 12:12:12";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -191,8 +200,8 @@ mod tests {
         fn test_keywords() {
             // Arrange
             let re = Regex::new(YAML_FRONTMATTER_KEYWORDS).unwrap();
-            let input = "tags:   [\"foo\", \"bar\"]\n";
-            let expected = "[\"foo\", \"bar\"]";
+            let input = "tags:   [\"foo\",    \"bar\", \"baz\",  \"boom\"]\n";
+            let expected = "[\"foo\",    \"bar\", \"baz\",  \"boom\"]";
 
             // Act
             let result = re.captures(input).unwrap().get(1).unwrap().as_str();
@@ -207,11 +216,12 @@ mod tests {
         #[test]
         fn test_identifier() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(YAML_FRONTMATTER_IDENTIFIER).unwrap();
+            let input = "identifier:   \"20241212T121212\"\n";
+            let expected = "\"20241212T121212\"";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -227,19 +237,20 @@ mod tests {
         #[test]
         fn test_container() {
             let re = Regex::new(TOML_FRONTMATTER_CONTAINER).unwrap();
-            assert!(re.is_match("\n+++\n"));
-            assert!(!re.is_match("\n++\n"));
-            assert!(!re.is_match("\n++++\n"));
+            assert!(re.is_match("+++\n"));
+            assert!(!re.is_match("++\n"));
+            assert!(!re.is_match("++++\n"));
         }
 
         #[test]
         fn test_title() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TOML_FRONTMATTER_TITLE).unwrap();
+            let input = "title =  \"my-Test : A N3w title\"\n";
+            let expected = "\"my-Test : A N3w title\"";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -251,11 +262,12 @@ mod tests {
         #[test]
         fn test_date() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TOML_FRONTMATTER_DATE).unwrap();
+            let input = "date  = 2024-12-12 @ 12:12:12\n";
+            let expected = "2024-12-12 @ 12:12:12";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -267,11 +279,12 @@ mod tests {
         #[test]
         fn test_keywords() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TOML_FRONTMATTER_KEYWORDS).unwrap();
+            let input = "tags =   [\"foo\",    \"bar\", \"baz\",  \"boom\"]\n";
+            let expected = "[\"foo\",    \"bar\", \"baz\",  \"boom\"]";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -283,11 +296,12 @@ mod tests {
         #[test]
         fn test_identifier() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TOML_FRONTMATTER_IDENTIFIER).unwrap();
+            let input = "identifier   =   \"20241212T121212\"\n";
+            let expected = "\"20241212T121212\"";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -303,11 +317,12 @@ mod tests {
         #[test]
         fn test_title() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TEXT_FRONTMATTER_TITLE).unwrap();
+            let input = "#+title:   my-Test_ A N3w title\n";
+            let expected = "my-Test_ A N3w title";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -319,11 +334,12 @@ mod tests {
         #[test]
         fn test_date() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TEXT_FRONTMATTER_DATE).unwrap();
+            let input = "#+date:  [2024-12-12 Thu 12:12]\n";
+            let expected = "[2024-12-12 Thu 12:12]";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -335,11 +351,12 @@ mod tests {
         #[test]
         fn test_keywords() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TEXT_FRONTMATTER_KEYWORDS).unwrap();
+            let input = "#+filetags:   :foo:bar:baz:boom:\n";
+            let expected = ":foo:bar:baz:boom:";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
@@ -351,11 +368,12 @@ mod tests {
         #[test]
         fn test_identifier() {
             // Arrange
-            let input = todo!();
-            let expected = todo!();
+            let re = Regex::new(TEXT_FRONTMATTER_IDENTIFIER).unwrap();
+            let input = "#+identifier:   20241212T121212\n";
+            let expected = "20241212T121212";
 
             // Act
-            let result = todo!();
+            let result = re.captures(input).unwrap().get(1).unwrap().as_str();
 
             // Assert
             assert_eq!(
