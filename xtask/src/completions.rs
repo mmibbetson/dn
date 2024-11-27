957c687a2151d1b7414e72617f5a624641c907be
Matthew Ibbetson <mmibbetson@gmail.com>
2024-11-27 16:31:34 +0200

    feat: add xtask pattern to generate shell completions

---
+++ xtask/src/completions.rs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2024 Matthew Mark Ibbetson
+// SPDX-FileContributor: Matthew Mark Ibbetson
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::{fs, path::Path};
+
+use dn::cli::Cli;
+
+use clap::CommandFactory;
+use clap_complete::{
+    aot::{Bash, Elvish, Fish, PowerShell, Zsh},
+    generate_to,
+};
+use clap_complete_nushell::Nushell;
+
+pub fn gen() {
+    let base_dir = Path::new("");
+    let completions_dir = base_dir.join("completions");
+    fs::create_dir_all(&completions_dir).unwrap();
+
+    let mut cmd = Cli::command();
+    generate_to(Bash, &mut cmd, "dn", &completions_dir).unwrap();
+    generate_to(Zsh, &mut cmd, "dn", &completions_dir).unwrap();
+    generate_to(Fish, &mut cmd, "dn", &completions_dir).unwrap();
+    generate_to(Elvish, &mut cmd, "dn", &completions_dir).unwrap();
+    generate_to(Nushell, &mut cmd, "dn", &completions_dir).unwrap();
+    generate_to(PowerShell, &mut cmd, "dn", &completions_dir).unwrap();
+}
---
+++ xtask/src/main.rs
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: 2024 Matthew Mark Ibbetson
+// SPDX-FileContributor: Matthew Mark Ibbetson
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+use clap::{Parser, Subcommand};
+
+mod completions;
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate shell completion files
+    Completions,
+}
+
+fn main() {
+    let Cli { command } = Cli::parse();
+
+    env::set_current_dir(project_root()).unwrap();
+
+    match command {
+        Commands::Completions => completions::gen(),
+    }
+}
+
+fn project_root() -> PathBuf {
+    Path::new(
+        &env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned()),
+    )
+    .ancestors()
+    .nth(1)
+    .unwrap()
+    .to_path_buf()
+}
